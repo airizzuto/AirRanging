77e38a251bb1e6ae520fa777e6e6c9d197625070
airizzuto <agustinrizzuto@hotmail.com>
2021-05-15 13:46:21 -0300

    New: web-ui skeleton

---
+++ web-ui/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Style from "../styles/Header.module.scss";
+
+export const Header = () => {
+  return (
+    <div className={Style.Header}>
+
+    </div>
+  );
+};
