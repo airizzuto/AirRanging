15b82d5302d3747686149cef4fe31ec232858e56
airizzuto <agustinrizzuto@hotmail.com>
2021-05-17 13:34:04 -0300

    New: mock db

--- AirRangingAPI/Data/MockDb.cs
+++ AirRangingAPI/Data/MockDb.cs
@@ -1,7 +1,57 @@
+using System.Collections.Generic;
+using AirRangingAPI.Enums;
+using AirRangingAPI.Models;
+
 namespace AirRangingAPI.Data
 {
   public class MockDb : IAirRangingDb
   {
-    
+    public Aircraft GetAircraftById(int id)
+    {
+      return new Aircraft {
+        Id=0, Icao="C152", Manufacturer="Cessna", Model="152",
+        AircraftType=AircraftTypes.FixedWing, EngineType=EngineTypes.Piston,
+        WeightClass=WeightCategories.Small, WakeCategory=WakeCategories.Light,
+        FuelType=FuelTypes.AvGas,
+        CruiseSpeed=107, FuelCapacity=26, MaxRange=415, ServiceCeiling=14700
+      };
+    }
+
+    public IEnumerable<Aircraft> GetAllAircraft()
+    {
+      var aircrafts = new List<Aircraft>
+      {
+        new Aircraft {
+          Id=0, Icao="C152", Manufacturer="Cessna", Model="152",
+          AircraftType=AircraftTypes.FixedWing, EngineType=EngineTypes.Piston,
+          WeightClass=WeightCategories.Small, WakeCategory=WakeCategories.Light,
+          FuelType=FuelTypes.AvGas,
+          CruiseSpeed=107, FuelCapacity=26, MaxRange=415, ServiceCeiling=14700
+        },
+        new Aircraft {
+          Id=1, Icao="C152", Manufacturer="Cessna", Model="152", Variant="Long-Range",
+          AircraftType=AircraftTypes.FixedWing, EngineType=EngineTypes.Piston,
+          WeightClass=WeightCategories.Small, WakeCategory=WakeCategories.Light,
+          FuelType=FuelTypes.AvGas,
+          CruiseSpeed=107, FuelCapacity=38, MaxRange=691, ServiceCeiling=14700
+        },
+        new Aircraft {
+          Id=2, Icao="C172", Manufacturer="Cessna", Model="172",
+          AircraftType=AircraftTypes.FixedWing, EngineType=EngineTypes.Piston,
+          WeightClass=WeightCategories.Small, WakeCategory=WakeCategories.Light,
+          FuelType=FuelTypes.AvGas,
+          CruiseSpeed=122, FuelCapacity=56, MaxRange=696, ServiceCeiling=13500
+        },
+        new Aircraft {
+          Id=3, Icao="A320", Manufacturer="Airbus", Model="320",
+          AircraftType=AircraftTypes.FixedWing, EngineType=EngineTypes.Jet,
+          WeightClass=WeightCategories.Large, WakeCategory=WakeCategories.Medium,
+          FuelType=FuelTypes.JetA, MTOW=172000,
+          CruiseSpeed=447, FuelCapacity=6400, MaxRange=3300, ServiceCeiling=39100
+        }
+      };
+
+      return aircrafts;
+    }
   }
 }
