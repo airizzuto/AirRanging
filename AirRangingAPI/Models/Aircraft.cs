84609194b4169fa72f6c5c4092b6b8f573de0f0a
airizzuto <agustinrizzuto@hotmail.com>
2021-05-17 13:34:22 -0300

    Fix: enum type naming

--- AirRangingAPI/Models/Aircraft.cs
+++ AirRangingAPI/Models/Aircraft.cs
@@ -1,18 +1,44 @@
+using System.ComponentModel.DataAnnotations;
+using AirRangingAPI.Enums;
+
 namespace AirRangingAPI.Models
 {
   public class Aircraft
   {
+    // TODO: Validation
+    [Key]
     public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(4)]
     public string Icao { get; set; }
+
+    [Required]
     public string Manufacturer { get; set; }
+
+    [Required]
     public string Model { get; set; }
-    public AircraftType AircraftType { get; set; }
-    public EngineType EngineType { get; set; }
-    public WeightCategory WeightClass { get; set; }
-    public WakeCategory WakeCategory { get; set; }
-    public FuelType FuelType { get; set; }
-    public decimal MaxFuel { get; set; }
+
+    public string Variant { get; set; }
+    public AircraftTypes AircraftType { get; set; }
+    public EngineTypes EngineType { get; set; }
+    public WeightCategories WeightClass { get; set; }
+    public WakeCategories WakeCategory { get; set; }
+    public FuelTypes FuelType { get; set; }
+
+    public int MTOW { get; set; }
+
+    [Required]
+    public int CruiseSpeed { get; set; }
+
+    [Required]
+    public decimal FuelCapacity { get; set; }
     // TODO: Converted Fuel Weight <-> Volume
+
+    [Required]
     public decimal MaxRange { get; set; }
+
+    [Required]
+    public int ServiceCeiling { get; set; }
   }
 }
