543bf5cf9bbd9be4dd6d0dba54b76c8dc3496592
airizzuto <agustinrizzuto@hotmail.com>
2021-05-17 21:09:25 -0300

    Update: aircraft controller skeleton

--- AirRangingAPI/Controllers/AircraftsController.cs
+++ AirRangingAPI/Controllers/AircraftsController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using AirRangingAPI.Domain.Models;
 using AirRangingAPI.Domain.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,10 +15,32 @@ namespace AirRangingAPI.Controllers
         _aircraftService = aircraftService;
     }
     
+    // TODO: replace with model
+
+    // GET api/aircrafts
     [HttpGet]
     public ActionResult<IEnumerable<string>> Get()
     {
       return new string[] {"C152", "C172", "A320", "T-50"};
     }
+
+    // GET api/aircrafts/5
+    [HttpGet("{id}")]
+    public ActionResult<string> Get(int id)
+    {
+      return "C152";
+    }
+
+    // POST api/aircrafts
+    [HttpPost]
+    public void Post([FromBody] string value) { }
+
+    // PUT api/aircrafts/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value) { }
+
+    // DELETE api/aircrafts/5
+    [HttpDelete("{id}")]
+    public void Delete(int id) { }
   }
 }
