dbd4862ece61f050e87851477e385e9135046613
airizzuto <agustinrizzuto@hotmail.com>
2021-06-30 10:57:53 -0300

    Add: scoped service

--- Server/App/Extensions/Configurations/EmailerServiceConfiguration.cs
+++ Server/App/Extensions/Configurations/EmailerServiceConfiguration.cs
@@ -9,11 +9,12 @@ namespace App.Extensions.Configurations
         public static void ConfigureEmailerService(
             this IServiceCollection services, IConfiguration configuration)
         {
-            var emailConfiguration = configuration
+            var emailConfig = configuration
                 .GetSection(nameof(EmailConfiguration))
                 .Get<EmailConfiguration>();
             
-            services.AddSingleton(emailConfiguration);
+            services.AddSingleton(emailConfig);
+            services.AddScoped<IEmailSender, EmailSender>();
         }
     }
 }
