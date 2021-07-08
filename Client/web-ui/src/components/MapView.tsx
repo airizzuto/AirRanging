ec7469334f72ebbb8073b6dacaaacca84dfbeb30
airizzuto <agustinrizzuto@hotmail.com>
2021-07-08 20:36:38 -0300

    Updated: renamed Main to MapView

---
+++ Client/web-ui/src/components/MapView.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Style from "./MapView.module.scss"
+
+const MapView = (): JSX.Element => {
+  return(
+    <div className={Style.Main}>
+
+    </div>
+  )
+}
+
+export default MapView;
