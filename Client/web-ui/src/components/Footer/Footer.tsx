ca6c17e7b7318b3e7359386106bd1c2791df5f40
airizzuto <agustinrizzuto@hotmail.com>
2021-07-08 21:03:38 -0300

    Added: footer areas

--- Client/web-ui/src/components/Footer/Footer.tsx
+++ Client/web-ui/src/components/Footer/Footer.tsx
@@ -5,9 +5,17 @@ import Style from "./Footer.module.scss";
 const Footer = (): JSX.Element => {
   return (
     <div className={Style.Footer}>
-      <a href="/">Help</a>
-      <a href="/">About</a>
-      <a href="/">Contact</a>
+
+      <div className={Style.Copyright}>
+        Copyright
+      </div>
+
+      <div className={Style.Links}>
+        <a href="/">Help</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+      </div>
+
     </div>
   );
 };
