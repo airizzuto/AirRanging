867b50f59249f06f5612ecc9b0f6bfce51eb4ed7
airizzuto <agustinrizzuto@hotmail.com>
2021-07-13 12:03:05 -0300

    Added: initial table implementation

--- Client/web-ui/src/components/AircraftEditView/AircraftEditView.tsx
+++ Client/web-ui/src/components/AircraftEditView/AircraftEditView.tsx
@@ -1,10 +1,70 @@
-import React from 'react'
-import Style from "./AircraftEditView.module.scss"
+import React from 'react';
+
+import { AircraftType, EngineType } from "../../models/enums/AircraftEnums";
+import Table from '../Table/Table';
+import Style from "./AircraftEditView.module.scss";
+
+interface IAircraftDetails {
+  icaoId: string;
+  manufacturer: string;
+  model: string;
+  variant: string;
+  aircraftType: AircraftType;
+  engineType: EngineType;
+  engineCount: number;
+  maxRange: number;
+}
 
 const AircraftEditView = (): JSX.Element => {
+  const data = React.useMemo(
+    () => [],
+    []
+  )
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'ICAO',
+        accessor: 'icaoId',
+      },
+      {
+        Header: 'Manufacturer',
+        accessor: 'manufacturer',
+      },
+      {
+        Header: 'Model',
+        accessor: 'model',
+      },
+      {
+        Header: 'Variant',
+        accessor: 'variant',
+      },
+      {
+        Header: 'AircraftType',
+        accessor: 'aircraftType',
+      },
+      {
+        Header: 'EngineType',
+        accessor: 'engineType',
+      },
+      {
+        Header: 'EngineCount',
+        accessor: 'engineCount',
+      },
+      {
+        Header: 'MaxRange',
+        accessor: 'maxRange',
+      },
+    ],
+    []
+  )
+
   return (
     <div className={Style.AircraftEditView}>
-      
+      {/*TODO: Search bar*/}
+      <h1>Browse Aircrafts</h1>
+      <br/>
+      <Table columns={columns} data={data} />
     </div>
   )
 }
