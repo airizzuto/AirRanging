ebb1e2458ed2fd2c56b57f482998d94b83d903ca
airizzuto <agustinrizzuto@hotmail.com>
2021-07-19 16:41:13 -0300

    Added: break line, better padding
    Fixed: component drag

--- Client/web-ui/src/components/Modals/Modal.tsx
+++ Client/web-ui/src/components/Modals/Modal.tsx
@@ -12,27 +12,24 @@ interface Props {
   children: JSX.Element
 }
 
-
+// TODO: Draggable bounds, dragabble area
 const Modal = ({ label, handleClose, show, children }: Props): JSX.Element => {
   const showHideClassName = show ? {display: "block"} : {display: "none"};
 
   return (
     <Draggable
-      axis="both"
-      handle=".ModalHeader"
-      defaultPosition={{x: 200, y: 50}}
-      scale={1}
+      defaultPosition={{x: 200, y: 60}}
     >
       <div className={Style.Modal} style={showHideClassName}>
         <div className={Style.ModalHeader}>
           <h1 className={Style.ModalTitle}>{label}</h1>
           <ExitButton handleClick={handleClose}/>
         </div>
-
+        <hr />
         <div className={Style.ModalContent}>
           {children}
         </div>
-    </div>
+      </div>
     </Draggable>
   );
 };
