108a27e9361c2d68930593b1d239307384c854fb
airizzuto <agustinrizzuto@hotmail.com>
2021-07-21 12:32:54 -0300

    Added: close on click outside hook

--- Client/web-ui/src/components/Modals/FixedModal.tsx
+++ Client/web-ui/src/components/Modals/FixedModal.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+
+import useOnClickOutside from "../../hooks/useClickOutside";
 import DecoratedButton from "../Buttons/DecoratedButton";
 
 import ExitButton from "../Buttons/ExitButton";
@@ -14,11 +17,15 @@ interface Props {
 export default function Modal({
   label, visible, handleModalClose, children
 }: Props): JSX.Element {
+  const ref = React.useRef(null);
+
   const showHideClassName = visible ? { display: "grid" } : { display: "none" };
+
+  useOnClickOutside(ref, handleModalClose);
 
   return (
     <div className={Style.Modal} style={showHideClassName}>
-      <div className={Style.ModalMain}>
+      <div className={Style.ModalMain} ref={ref}>
         <div className={Style.ModalHeader}>
           <h1 className={Style.ModalTitle}>{label}</h1>
           <div className={Style.CloseButton}>
--- Client/web-ui/src/hooks/useClickOutside.tsx
+++ Client/web-ui/src/hooks/useClickOutside.tsx
@@ -1,25 +1,37 @@
-import React, { useEffect } from "react";
+import { RefObject, useEffect } from "react";
 
-interface ClickOutsideProps {
-  onClick(e: MouseEvent): void;
-  ref: React.RefObject<HTMLElement>;
+type AnyEvent = MouseEvent | TouchEvent;
+
+function useOnClickOutside<T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>,
+
+  handler: (event: AnyEvent) => void
+) {
+  useEffect(() => {
+    const listener = (event: AnyEvent) => {
+      const el = ref?.current;
+
+      // Do nothing if clicking ref's element or descendent elements
+
+      if (!el || el.contains(event.target as Node)) {
+        return;
+      }
+
+      handler(event);
+    };
+
+    document.addEventListener(`mousedown`, listener);
+
+    document.addEventListener(`touchstart`, listener);
+
+    return () => {
+      document.removeEventListener(`mousedown`, listener);
+
+      document.removeEventListener(`touchstart`, listener);
+    };
+
+    // Reload only if ref or handler changes
+  }, [ref, handler]);
 }
 
-const useClickOutside = ({ref, onClick}: ClickOutsideProps ) => {
-  useEffect(() => {
-    if (!ref?.current) {
-      return;
-    }
-    const handleClickOutside = (e: MouseEvent) => {
-      if (onClick && !ref.current.contains(e.target as Node)) {
-        onClick(e);
-      }
-    };
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [ref, onClick]);
-};
-
-export default useClickOutside;
+export default useOnClickOutside;
