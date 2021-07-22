bbcc4bb134f6f206490583465d63a5b798137013
airizzuto <agustinrizzuto@hotmail.com>
2021-07-22 12:12:37 -0300

    Added: registration validator skeleton

---
+++ Client/web-ui/src/helpers/registrationValidators.ts
@@ -0,0 +1,15 @@
+
+export const isUserValid = (): boolean => {
+  // TODO: user validation
+  return false;
+};
+
+export const isEmailValid = (): boolean => {
+  // TODO: email validation
+  return false;
+};
+
+export const isPasswordValid = (): boolean => {
+  // TODO: password validation
+  return false;
+};
