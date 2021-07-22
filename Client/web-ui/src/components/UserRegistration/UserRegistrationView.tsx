6c4c3bff111878a25fbae4c55e0fa15f0ab445e7
airizzuto <agustinrizzuto@hotmail.com>
2021-07-22 12:52:37 -0300

    Updated: registration style

--- Client/web-ui/src/components/UserRegistration/UserRegistrationView.tsx
+++ Client/web-ui/src/components/UserRegistration/UserRegistrationView.tsx
@@ -6,43 +6,52 @@ const UserRegistrationView = () => {
   const handleClick = () => { return; }; {/* TODO */}
 
   return (
-    <div>
-      <h1>Register</h1>
+    <div className={Style.RegistrationView}>
+      <h1>User Registration</h1>
+
       <hr />
+
       <div>
         <form className={Style.RegistrationForm}>
-          <div className={Style.FormGroup}>
-            <label>First Name:</label>
-            <input type="text" name="FirstName" />
-            <span>{/* TODO: Validation output here */}</span>
-          </div>
+          <div className={Style.RegistrationFields}>
 
-          <div className={Style.FormGroup}>
-            <label>Last Name:</label>
-            <input type="text" name="LastName" />
-            <span>{/* TODO: Validation output here */}</span>
-          </div>
+            <div className={Style.FormGroup}>
+              <label>First Name:</label>
+              <input type="text" name="FirstName" />
+              <span>{/* TODO: Validation output here */}</span>
+            </div>
 
-          <div className={Style.FormGroup}>
-            <label>Email:</label>
-            <input type="email" name="Email" />
-            <span>{/* TODO: Validation output here */}</span>
-          </div>
+            <div className={Style.FormGroup}>
+              <label>Last Name:</label>
+              <input type="text" name="LastName" />
+              <span>{/* TODO: Validation output here */}</span>
+            </div>
 
-          <div className={Style.FormGroup}>
-            <label>Password:</label>
-            <input type="password" name="Password"/>
-            <span>{/* TODO: Validation output here */}</span>
-          </div>
+            <div className={Style.FormGroup}>
+              <label>Email:</label>
+              <input type="email" name="Email" />
+              <span>{/* TODO: Validation output here */}</span>
+            </div>
 
-          <div className={Style.FormGroup}>
-            <label>Confirm Password</label>
-            <input type="password" name="ConfirmPassword" />
-            <span>{/* TODO: Validation output here */}</span>
+            <div className={Style.FormGroup}>
+              <label>Password:</label>
+              <input type="password" name="Password"/>
+              <span>{/* TODO: Validation output here */}</span>
+            </div>
+
+            <div className={Style.FormGroup}>
+              <label>Confirm Password:</label>
+              <input type="password" name="ConfirmPassword" />
+              <span>{/* TODO: Validation output here */}</span>
+            </div>
+
           </div>
 
           <div className={Style.SubmitButton}>
-          <DecoratedButton buttonText="Submit" handleClick={() => handleClick}/>
+            <DecoratedButton 
+              buttonText="Submit"
+              handleClick={() => handleClick}
+            />
         </div>
         </form>
       </div>
