1ef1919be1299fa62c92575b5e1c45307ef1891c
airizzuto <agustinrizzuto@hotmail.com>
2021-07-22 12:13:17 -0300

    Added: scaffolded registration page

--- Client/web-ui/src/App.tsx
+++ Client/web-ui/src/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import { useModalClose } from "./hooks/useModalClose";
+import { useModalToggle } from "./hooks/useModalToggle";
 
-import "./App.scss";
+import MapView from "./components/MapView/MapView";
 import AircraftEditView from "./components/AircraftEditView/AircraftEditView";
+import UserRegistrationView from "./components/UserRegistration/UserRegistrationView";
+
 import Header from "./components/Header/Header";
 import Login from "./components/Login/Login";
-import MapView from "./components/MapView/MapView";
 import Footer from "./components/Footer/Footer";
-import { useModalToggle } from "./hooks/useModalToggle";
+
+import "./App.scss";
 
 const App = (): JSX.Element => {
   const [showLogin, setShowLogin] = React.useState(false);
@@ -29,7 +32,7 @@ const App = (): JSX.Element => {
             {/* <AirportsEditView /> */}
           </Route>
           <Route exact path="/registration">
-            {/* <UserSignup /> */}
+            <UserRegistrationView />
           </Route>
           <Route exact path="/forgotpass">
             {/* <ForgotPassword /> */}
---
+++ Client/web-ui/src/components/UserRegistration/UserRegistrationView.tsx
@@ -0,0 +1,53 @@
+import DecoratedButton from '../Buttons/DecoratedButton';
+
+import Style from "./UserRegistration.module.scss";
+
+const UserRegistrationView = () => {
+  const handleClick = () => { return; }; {/* TODO */}
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <hr />
+      <div>
+        <form className={Style.RegistrationForm}>
+          <div className={Style.FormGroup}>
+            <label>First Name:</label>
+            <input type="text" name="FirstName" />
+            <span>{/* TODO: Validation output here */}</span>
+          </div>
+
+          <div className={Style.FormGroup}>
+            <label>Last Name:</label>
+            <input type="text" name="LastName" />
+            <span>{/* TODO: Validation output here */}</span>
+          </div>
+
+          <div className={Style.FormGroup}>
+            <label>Email:</label>
+            <input type="email" name="Email" />
+            <span>{/* TODO: Validation output here */}</span>
+          </div>
+
+          <div className={Style.FormGroup}>
+            <label>Password:</label>
+            <input type="password" name="Password"/>
+            <span>{/* TODO: Validation output here */}</span>
+          </div>
+
+          <div className={Style.FormGroup}>
+            <label>Confirm Password</label>
+            <input type="password" name="ConfirmPassword" />
+            <span>{/* TODO: Validation output here */}</span>
+          </div>
+
+          <div className={Style.SubmitButton}>
+          <DecoratedButton buttonText="Submit" handleClick={() => handleClick}/>
+        </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default UserRegistrationView;
