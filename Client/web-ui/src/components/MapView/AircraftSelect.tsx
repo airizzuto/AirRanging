fff29822b7a43e109b5829c1c8c5856dfe9fad33
airizzuto <agustinrizzuto@hotmail.com>
2021-07-26 17:34:23 -0300

    Added: component initial preparation

---
+++ Client/web-ui/src/components/MapView/AircraftSelect.tsx
@@ -0,0 +1,15 @@
+
+/* React select documentation https://react-select.com/home */
+// TODO:
+
+const AircraftSelect = () => {
+  return (
+    <div>
+      {/* TODO: table selection */}
+      {/* TODO: search bar */}
+      {/* TODO: confirm selection */}
+    </div>
+  );
+};
+
+export default AircraftSelect;
