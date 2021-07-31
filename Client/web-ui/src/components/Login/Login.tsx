99b2d094c392402f19adc9222ae23eac5e2864f3
airizzuto <agustinrizzuto@hotmail.com>
2021-07-31 13:49:48 -0300

    Updated: refactored to use formik and validators

--- Client/web-ui/src/components/Login/Login.tsx
+++ Client/web-ui/src/components/Login/Login.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Formik, Form, Field, FormikHelpers } from 'formik';
+
 import userService from "../../services/userService";
 import aircraftService from "../../services/aircraftService";
 
@@ -7,6 +9,7 @@ import FixedModal from "../Modals/FixedModal";
 
 import Style from "./Login.module.scss";
 import CheckboxStyle from "../../styles/components/_checkbox.module.scss";
+import { userLoginSchema } from "../../validators/userValidators";
 
 interface Props {
   showLogin: boolean;
@@ -14,40 +17,20 @@ interface Props {
   setUser: React.Dispatch<React.SetStateAction<null>>;
 }
 
+interface Values {
+  email: string;
+  password: string;
+}
+
 export default function LoginModal({ showLogin, handleClose, setUser }: Props): JSX.Element {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
 
-  const handleEmailChange = (event: { 
-    preventDefault: () => void; target: { value: React.SetStateAction<string>; }; 
-  }) => {
-    event.preventDefault();
-    setEmail(event.target.value);
-  };
-
-  const handlePasswordChange = (event: { 
-    preventDefault: () => void; target: { value: React.SetStateAction<string>; };
-  }) => {
-    event.preventDefault();
-    setPassword(event.target.value);
-  };
-
-  const handleSubmitLogin = async (event: { preventDefault: () => void; }) => {
-    event.preventDefault();
-
+  const handleSubmit = async ({email, password}: Values) => {
     try {
-      const user = await userService.login({
-        email, password
-      });
+      const user = await userService.login({ email, password });
 
-      window.localStorage.setItem(
-        "userToken", JSON.stringify(user)
-      );
+      window.localStorage.setItem("userToken", JSON.stringify(user));
       aircraftService.setToken(user.token);  // For aircraft requests with authentication requirements
       setUser(user);
-      // Resets fields
-      setEmail("");
-      setPassword("");
     } catch (error) {
       // TODO: user error notification
       console.log(error.message);
@@ -60,54 +43,67 @@ export default function LoginModal({ showLogin, handleClose, setUser }: Props): 
       visible={showLogin}
       handleModalClose={handleClose}
     >
-      <form className={Style.LoginForm} onSubmit={handleSubmitLogin}>
+      <Formik 
+        initialValues={{ email: "", password: ""}}
+        validationSchema={userLoginSchema}
+        onSubmit={async (values: Values, { setSubmitting }: FormikHelpers<Values>) => {
+          await handleSubmit(values);
+          setSubmitting(false);
+        }}
+      >
+        {({isSubmitting}) => (
+          <Form className={Style.LoginForm}>
 
-        <div className={Style.LoginFields}>
-          <div className={Style.FormGroup}>
-            <label>Email:</label>
-            <input type="email"
-              name="email"
-              value={email}
-              onChange={handleEmailChange}
-            />
-          </div>
-          <div className={Style.FormGroup}>
-            <label>Password:</label>
-            <input type="password"
-              name="password"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-          </div>
-        </div>
+            <div className={Style.LoginFields}>
+              <div className={Style.FormGroup}>
+                <label htmlFor="email">Email:</label>
+                <Field type="email" name="email"
+                  placeholder={"useremail@emailprovider.com"}
+                />
+              </div>
+              <div className={Style.FormGroup}>
+                <label htmlFor="password">Password:</label>
+                <Field type="password" name="password" />
+              </div>
+            </div>
 
-        <div className={Style.Options}>
-          <div className={Style.RememberMe}>
-            <label className={CheckboxStyle.checkbox}>Remember Me
-              <input type="checkbox"/>
-              <span className={CheckboxStyle.checkmark}></span>
-            </label>
-          </div>
-          <div className={Style.ForgotPassword}>
-            <Link to="/forgotpass" onClick={handleClose}>
-              Forgot Password?
-            </Link>
-          </div>
-        </div>
-        
-        <div className={Style.Buttons}>
-          <div className={Style.LoginButton}>
-            <button type="submit" className={Style.LoginButton}>Login</button>
-          </div>
-          <p>OR</p>
-          <div className={Style.RegistrationButton}>
-            <Link to="/registration" onClick={handleClose}>
-              Register
-            </Link>
-          </div>
-        </div>
+            <div className={Style.Options}>
+              <div className={Style.RememberMe}>
+                <label className={CheckboxStyle.checkbox}>Remember Me
+                  <Field type="checkbox" name="rememberme"/>
+                  <span className={CheckboxStyle.checkmark}></span>
+                </label>
+              </div>
+              <div className={Style.ForgotPassword}>
+                <Link to="/forgotpass" onClick={handleClose}>
+                  Forgot Password?
+                </Link>
+              </div>
+            </div>
 
-      </form>
+            <div className={Style.Buttons}>
+              <div className={Style.LoginButton}>
+                <button 
+                  type="submit"
+                  disabled={isSubmitting}
+                  className={Style.LoginButton}
+                >
+                  Login
+                </button>
+              </div>
+              <p>OR</p>
+              <div className={Style.RegistrationButton}>
+                <Link to="/registration" onClick={handleClose}>
+                  Register
+                </Link>
+              </div>
+            </div>
+
+          </Form>
+
+        )}
+      </Formik>
+      
     </FixedModal>
   );
 }
