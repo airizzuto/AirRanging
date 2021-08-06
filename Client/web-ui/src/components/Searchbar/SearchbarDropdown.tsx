6fa09ae4767680023bddb674b98a492f7e831bdc
airizzuto <agustinrizzuto@hotmail.com>
2021-08-06 17:37:55 -0300

    Update: renamed component

---
+++ Client/web-ui/src/components/Searchbar/SearchbarDropdown.tsx
@@ -0,0 +1,26 @@
+
+/* React select documentation https://react-select.com/home */
+
+// TODO: styling
+
+const SearchbarDropdown = () => {
+
+  // const promiseOptions = async (value: string): Promise<unknown> =>
+  //   new Promise(resolve => {
+  //     setTimeout(async () => {
+  //       resolve(await searchFunction(value));
+  //     }, 1000);
+  // });
+
+  return (
+    <div>
+      {/* <AsyncSelect
+        cacheOptions
+        loadOptions={pro}
+        defaultOptions
+      /> */}
+    </div>
+  );
+};
+
+export default SearchbarDropdown;
