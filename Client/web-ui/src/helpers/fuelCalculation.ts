4751e4bba4b334034d8640ef667bf5de559bd8af
airizzuto <agustinrizzuto@hotmail.com>
2021-08-11 17:11:27 -0300

    Added: basic fuel calculations

---
+++ Client/web-ui/src/helpers/fuelCalculation.ts
@@ -0,0 +1,14 @@
+
+interface FuelProps {
+  maxRange: number,
+  fuelCapacity: number,
+  fuelLoaded: number
+}
+
+export const calculateRange = ({maxRange, fuelCapacity, fuelLoaded}: FuelProps): number => {
+  return ((fuelLoaded * maxRange) / fuelCapacity);
+};
+
+export const calculateRadiusOfAction = ({...fuelProps}: FuelProps): number => {
+  return calculateRange(fuelProps) / 2; // TODO: check calculation
+};
