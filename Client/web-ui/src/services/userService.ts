e5148607563defbfd1d6c93d2b2ca2df5e89542d
airizzuto <agustinrizzuto@hotmail.com>
2021-08-11 17:11:13 -0300

    Update: moved token requests to file

--- Client/web-ui/src/services/userService.ts
+++ Client/web-ui/src/services/userService.ts
@@ -44,48 +44,6 @@ const logout = () => {
   window.localStorage.removeItem("user");
 };
 
-
-/* Tokens endpoints
-/// <summary>
-/// Token controller endpoints:
-/// <para> RefreshToken  -  POST  -  api/tokens/refresh </para>
-/// <para> RevokeToken   -  POST  -  api/tokens/revoke  </para>
-/// </summary>
-*/
-
-export const refreshToken = async (token: string) => {
-  const refreshToken = localStorage.getItem("user.refreshToken");
-
-  if(!token || !refreshToken) {
-    return false;
-  }
-
-  const credentials = JSON.stringify({ accessToken: token, refreshToken: refreshToken });
-
-  try {
-    const config = {
-      headers: { "Content-Type": "application/json" }, 
-      observe: "response"
-    };
-
-    const response = await axios.post(
-      baseUrl + "/api/tokens/refresh",
-      credentials,
-      config,
-    );
-    const newToken = response.data.accessToken;
-    const newRefreshToken = response.data.refreshToken;
-    localStorage.setItem("user.token", newToken);
-    localStorage.setItem("user.refreshToken", newRefreshToken);
-
-    return true;
-  } catch (ex) {
-    return false;
-  }
-};
-
-// TODO: revoke on refresh token expiration
-
 export default {
   register,
   login,
