394f87e33cf15aea5bd4c33f9858db9d038016e4
airizzuto <agustinrizzuto@hotmail.com>
2021-08-18 12:16:57 -0300

    Added: documented function

--- Client/web-ui/src/helpers/aircraftHelper.ts
+++ Client/web-ui/src/helpers/aircraftHelper.ts
@@ -1,5 +1,12 @@
 import { AircraftData } from "../types/Aircraft/Aircraft";
 
+/**
+ * Adapts aircraft data to be used by the react-select searchbar.
+ * Assigns the data to value and a model property to label.
+ * @param data: aircraft data to use in search.
+ * @param labelProp: aircraft property to represent model in searchbar.
+ * @returns mapped data array to value containing element properties and a label to display it in search.
+ */
 export const mapAircraftToFilter = (
   data: AircraftData[],
   labelProp: string
