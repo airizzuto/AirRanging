358621c2563a715a5359e51a4d94b7477535b5da
airizzuto <agustinrizzuto@hotmail.com>
2021-08-17 21:41:42 -0300

    Update: map default center options

--- Client/web-ui/src/settings/google-maps/mapSettings.ts
+++ Client/web-ui/src/settings/google-maps/mapSettings.ts
@@ -13,4 +13,10 @@ export const DEFAULT_MAP_OPTIONS = {
   streetViewControl: false,
 };
 
+export const DEFAULT_MAP_CENTER = {
+  london: { lat: 51.507351, lng: -0.127758 },
+  buenosAires: { lat: -34.603683, lng: -58.381557 },
+  washington: { lat: 47.751076, lng: -120.740135 }
+};
+
 export const LIBRARIES: Libraries | undefined = ["places", "drawing"];
