e17ad8fd9dab306ab8dc3756d4a39c84d41371d9
airizzuto <agustinrizzuto@hotmail.com>
2021-08-20 12:05:20 -0300

    Added: fetch aircraft data

--- client/src/components/Pages/AircraftDetails/AircraftDetailsPage.tsx
+++ client/src/components/Pages/AircraftDetails/AircraftDetailsPage.tsx
@@ -1,17 +1,28 @@
 
+import React from "react";
+import { useParams } from "react-router-dom";
+import aircraftService from "../../../services/aircraftService";
+
+import { AircraftData } from "../../../types/Aircraft/Aircraft";
+
 import Style from "./AircraftDetailsPage.module.scss";
 
-const AircraftDetails = () => {
-  
+
+const AircraftDetails: React.FC = () => {
+  const { id }: any = useParams();
+  const [aircraft, setAircraft] = React.useState<AircraftData>();
   // const [isEditMode, setIsEditMode] = useState(false);
 
   // edit button activates edit mode if user is owner. if user not owner clones aircraft and activates edit mode
 
-  // 
+  React.useEffect(() => {
+    aircraftService.getAircraftById(id)
+      .then(response => setAircraft(response));
+  }, []);
 
   return (
     <div className={Style.AircraftDetailsContainer}>
-      <h1>Aircraft *MODEL* Details</h1>
+      <h1>Aircraft Details: {aircraft?.model} - {aircraft?.variant}</h1>
 
       <hr />
 
