8356930adcbabadd385a5481c6129c80d76d8cac
airizzuto <agustinrizzuto@hotmail.com>
2021-08-25 10:40:32 -0300

    Added: isUserOwner helper

--- client/src/helpers/userHelper.ts
+++ client/src/helpers/userHelper.ts
@@ -1,7 +1,14 @@
+import { AircraftData } from "../types/Aircraft/Aircraft";
 import { UserPublic } from "../types/User/User";
 
 export const getUserData = (): UserPublic | null => {
   const user = window.localStorage.getItem("user.username");
 
   return user ? { username: user } : null;
+};
+
+// TODO: fetch saved
+
+export const isUserOwner =  (aircraft: AircraftData): boolean => {
+  return getUserData()?.username === aircraft.authorUsername;
 };
