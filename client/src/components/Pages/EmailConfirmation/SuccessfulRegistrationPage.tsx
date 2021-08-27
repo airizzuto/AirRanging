af713b400dda420e800d2671040b7af428318029
airizzuto <agustinrizzuto@hotmail.com>
2021-08-27 17:26:38 -0300

    Updated: component style

--- client/src/components/Pages/EmailConfirmation/SuccessfulRegistrationPage.tsx
+++ client/src/components/Pages/EmailConfirmation/SuccessfulRegistrationPage.tsx
@@ -7,7 +7,7 @@ const SuccessfulRegistration: React.FC = (): React.ReactElement => {
     <div className="SuccessfulRegistration">
       <h1>Successful User Registration</h1>
 
-      <p>Please check your email for your email validation.</p>
+      <p>Please check your email to continue with your registration.</p>
     </div>
   );
 };
