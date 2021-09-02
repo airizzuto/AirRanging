3eec3449ffbe24082024d82d310144b007f39d4f
airizzuto <agustinrizzuto@hotmail.com>
2021-09-02 15:46:47 -0300

    Fixed: styling

--- client/src/components/Pages/ErrorPages/EmailConfirmationFail.tsx
+++ client/src/components/Pages/ErrorPages/EmailConfirmationFail.tsx
@@ -1,13 +1,17 @@
 import { useHistory } from "react-router-dom";
+
 import DecoratedButton from "../../Buttons/DecoratedButton";
+
+import "./EmailConfirmationFail.scss";
 
 const EmailConfirmationFail = () => {
 
   const history = useHistory();
 
   return (
-    <div>
+    <div className="EmailConfirmationFail">
       <p>Email already confirmed or expired.</p>
+
       <DecoratedButton
         style={"primary"}
         onClick={() => history.push("/login")}
