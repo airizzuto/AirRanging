b2568ba315fecad1e862b3c4d15907c716e2ced9
airizzuto <agustinrizzuto@hotmail.com>
2021-09-10 22:08:01 -0300

    Updated: save/unsave service to new implementation

--- client/src/services/aircraftService.ts
+++ client/src/services/aircraftService.ts
@@ -3,23 +3,6 @@ import { BASE_URL } from "../constants/globals";
 import { getStoredToken, isUserAuthenticated } from "../helpers/tokenHelper";
 import { AircraftData, NewAircraft } from "../types/Aircraft/Aircraft";
 
-/*
-/// <summary>
-/// Aircraft model controller endpoints:
-/// <para> GetAllAircrafts             - GET     api/aircrafts            </para>
-/// <para> GetAllAircraftsPaginated    - GET     api/aircrafts/paginated  </para>
-/// <para> GetAircraftByParameters     - GET     api/aircrafts/search     </para>
-/// <para> GetAircraftOwnedByUser      - GET     api/aircrafts/owned      </para>
-/// <para> GetAircraftId               - GET     api/aircrafts/5          </para>
-/// <para> CreateAircraft              - POST    api/aircrafts/create     </para>
-/// <para> CloneAircraft               - POST    api/aircrafts/5/clone    </para>
-/// <para> PartialUpdateAircraftId     - PUT     api/aircrafts/5          </para>
-/// <para> SaveAircraftId              - PUT     api/aircrafts/5/save     </para>
-/// <para> FullUpdateAircraftId        - PATCH   api/aircrafts/5          </para>
-/// <para> DeleteAircraftId            - DELETE  api/aircrafts/5          </para>
-/// </summary>
-*/
-
 const getAllAircrafts = async () => {
   try {
     const response = await axios.get(
@@ -76,11 +59,11 @@ const getAircraftsOwnedByUser = async () => {
 const getAircraftsSavedByUser = async () => {
   try {
     const config = {
-      headers: { Authorization: `Bearer ${getStoredToken()}` },
+      headers: { Authorization: `Bearer ${getStoredToken()}` }
     };
     
     const response = await axios.get(
-      BASE_URL + `/api/aircrafts/saved`,
+      BASE_URL + `/api/bookmarks/saved`,
       config
     );
   
@@ -106,8 +89,11 @@ const createAircraft = async (newAircraft: NewAircraft) => {
   try {
     if (await isUserAuthenticated()) {
       const config = {
-        headers: { Authorization: `Bearer ${getStoredToken()}` }
+        headers: [
+          { Authorization: `Bearer ${getStoredToken()}` },
+        ]
       };
+
       const response = await axios.post(
         BASE_URL + "/api/aircrafts/create",
         newAircraft,
@@ -123,38 +109,45 @@ const createAircraft = async (newAircraft: NewAircraft) => {
 
 const saveAircraft = async (aircraftId: string) => {
   try {
-    const config = {
-      headers: { Authorization: `Bearer ${getStoredToken()}` }
-    };
-  
-    const response = await axios.put(
-      BASE_URL + `/api/aircrafts/${aircraftId}/save`,
-      aircraftId,
-      config
-    );
-  
-    // return response.status;
-    return response.data;
+    if (await isUserAuthenticated()) {
+      const config = {
+        headers: [
+          { Authorization: `Bearer ${getStoredToken()}` },
+        ]
+      };
+      
+      //FIXME: error parsing payload data
+      const response = await axios.post(
+        BASE_URL + "/api/bookmarks",
+        aircraftId,
+        config
+      );
+
+      // return response.status;
+      return response.data;
+    }
   } catch(error) {
     console.log(error);
   }
 };
 
-// TODO: unsave aircraft
 const unsaveAircraft = async (aircraftId: string) => {
   try {
-    const config = {
-      headers: { Authorization: `Bearer ${getStoredToken()}` }
-    };
-  
-    const response = await axios.put(
-      BASE_URL + `/api/aircrafts/${aircraftId}/unsave`,
-      aircraftId,
-      config
-    );
-  
-    // return response.status;
-    return response.data;
+    if (await isUserAuthenticated()) {
+      const config = {
+        headers: [
+          { Authorization: `Bearer ${getStoredToken()}` },
+        ]
+      };
+    
+      const response = await axios.delete(
+        BASE_URL + `/api/bookmarks/${aircraftId}`,
+        config
+      );
+    
+      // return response.status;
+      return response.data;
+    }
   } catch(error) {
     console.log(error);
   }
@@ -163,7 +156,7 @@ const unsaveAircraft = async (aircraftId: string) => {
 const editAircraft = async (aircraftId: string, aircraftUpdated: AircraftData) => {
   try {
     const config = {
-      headers: { Authorization: `Bearer ${getStoredToken()}` }
+      headers: { Authorization: `Bearer ${getStoredToken()}` },
     };
   
     const response = await axios.put(
@@ -182,7 +175,7 @@ const editAircraft = async (aircraftId: string, aircraftUpdated: AircraftData) =
 const deleteAircraft = async (aircraftId: string) => {
   try {
     const config = {
-      headers: { Authorization: `Bearer ${getStoredToken()}` }
+      headers: { Authorization: `Bearer ${getStoredToken()}` },
     };
   
     const response = await axios.delete(
