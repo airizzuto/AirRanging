c388d747d82dc9dc05b9cf562cb31a6ca74168e5
airizzuto <agustinrizzuto@hotmail.com>
2021-09-20 22:07:45 -0300

    Added: documentation

--- client/src/utils/propsToLabel.ts
+++ client/src/utils/propsToLabel.ts
@@ -9,6 +9,13 @@ interface UtilProps {
   separator?: string
 }
 
+/**
+ * Converts passed props to a single string.
+ * @param props Props to be concatenated into a single string.
+ * @param undefinedReplacement String to replace the prop with in case the prop element is undefined.
+ * @param separator String to be used as separator in between properties
+ * @returns All the properties in a single string.
+ */
 const propsToLabel = ({
   props, undefinedReplacement, separator
 }: UtilProps): string => {
