923cc58288c2d0a5076cf19932bfd5879f515149
airizzuto <agustinrizzuto@hotmail.com>
2021-09-27 16:39:10 -0300

    Added: selected aircraft info placeholder

--- client/src/components/InformationOverlays/AircraftInfoSection.tsx
+++ client/src/components/InformationOverlays/AircraftInfoSection.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
 import { AircraftData } from '../../types/Aircraft/Aircraft';
+import { Button } from '../Generics/Buttons/Button';
 
 import Style from "./AircraftInfoSections.module.scss";
 
 interface Props {
-  aircraftSelected: AircraftData[] | null;
+  aircraftSelected: AircraftData | null;
 }
 
-const AircraftInfoSelection: React.FC<Props> = (aircraftSelected) => {
+const AircraftInfoSelection: React.FC<Props> = ({aircraftSelected}) => {
   return (
     <div className={Style.Container}>
-      {/* TODO: Info fields */}
+      <label htmlFor="">Aircraft Selected</label>
+      aircraftSelected 
+      ? <span>{aircraftSelected?.model} {aircraftSelected?.variant}</span>
+      : <Button style={"undecorated"}>Select</Button>
     </div>
   );
 };
