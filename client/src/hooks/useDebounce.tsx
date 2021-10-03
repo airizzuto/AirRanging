095aa64598e88f37974f25ce61bde2c4bc952c8c
airizzuto <agustinrizzuto@hotmail.com>
2021-10-03 13:22:17 -0300

    Added: scaffolded hook

---
+++ client/src/hooks/useDebounce.tsx
@@ -0,0 +1,14 @@
+
+/* 
+  1. TODO: debounce effect
+  2. TODO: apply to any function that uses an user input to trigger an api request (search filter)
+*/
+const useDebounce = () => {
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default useDebounce;
