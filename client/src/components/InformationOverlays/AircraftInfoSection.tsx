6e2de9b9e0a134378271607aed40f06f3a69d382
airizzuto <agustinrizzuto@hotmail.com>
2021-10-12 17:28:19 -0300

    Updated: aircraft info section logic

--- client/src/components/InformationOverlays/AircraftInfoSection.tsx
+++ client/src/components/InformationOverlays/AircraftInfoSection.tsx
@@ -1,22 +1,49 @@
 import React from 'react';
 
-import { AircraftWithSocials } from '../../types/Aircraft/Aircraft';
-import { Button } from '../Generics/Buttons/Button';
+import { AircraftSelected } from '../../types/Aircraft/Aircraft';
 
 import Style from "./AircraftInfoSections.module.scss";
 
 interface Props {
-  aircraftSelected: AircraftWithSocials | null;
+  aircraftSelected: AircraftSelected | null;
 }
 
 const AircraftInfoSelection: React.FC<Props> = ({aircraftSelected}) => {
   return (
-    <div className={Style.Container}>
-      <label htmlFor="">Aircraft Selected</label>
-      aircraftSelected 
-      ? <span>{aircraftSelected?.model} {aircraftSelected?.variant}</span>
-      : <Button style={"undecorated"}>Select</Button>
-    </div>
+    !aircraftSelected
+    ? <div className={Style.Container}>
+        <div className={Style.Header}>
+          <h1>NO AIRCRAFT SELECTED</h1>
+        </div>
+        <div className={Style.Main}>
+          <span>NOT AVAILABLE</span>
+        </div>
+      </div>
+    : <div className={Style.Container}>
+        <div className={Style.Header}>
+          <h1>{aircraftSelected.manufacturer}</h1>
+          <h2>{aircraftSelected.model} - {aircraftSelected.variant}</h2>
+        </div>
+
+        <table>
+          <tr>
+            <td>Cruise Speed:</td>
+            <td>WIP</td>
+          </tr>
+          <tr>
+            <td>Altitude:</td>
+            <td>WIP</td>
+          </tr>
+          <tr>
+            <td>Endurance:</td>
+            <td>WIP</td>
+          </tr>
+          <tr>
+            <td>Max Range:</td>
+            <td>{aircraftSelected.currentMaxRange}</td>
+          </tr>
+        </table>
+      </div>
   );
 };
 
