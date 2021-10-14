3d0923d8554f80ea3317ef04bc5dde5bccefe8a7
airizzuto <agustinrizzuto@hotmail.com>
2021-10-14 17:25:28 -0300

    Added: reset filter to default helper

--- client/src/helpers/aircraftsFilters.ts
+++ client/src/helpers/aircraftsFilters.ts
@@ -1,14 +1,15 @@
 
 import { AircraftWithSocials } from "../types/Aircraft/Aircraft";
+import { AircraftSearchOptions } from "../types/Aircraft/AircraftEnums";
 import { Filters, AircraftsSets } from "../types/Aircraft/Filter";
 
 export const filterSets = async (
-    data: AircraftWithSocials[],
-    dataSet: AircraftWithSocials[],
-    filter: keyof AircraftsSets
+  data: AircraftWithSocials[],
+  dataSet: AircraftWithSocials[],
+  filter: keyof AircraftsSets
 ): Promise<AircraftWithSocials[]> => {
-    if (filter && dataSet) data.concat(...dataSet);
-    return data;
+  if (filter && dataSet) data.concat(...dataSet);
+  return data;
 };
 
 /**
@@ -18,8 +19,22 @@ export const filterSets = async (
  * @returns Filtered data.
  */
 export const filterSearch = async (
-    data: AircraftWithSocials[],
-    filter: Filters,
+  data: AircraftWithSocials[],
+  filter: Filters
 ): Promise<AircraftWithSocials[]> => {
-    return data.filter(aircraft => aircraft[filter.field as keyof AircraftWithSocials] === filter.search);
+  return data.filter((aircraft) =>
+    aircraft[filter.field as keyof AircraftWithSocials] === filter.search
+  );
+};
+
+/**
+ * Reset filter to default state.
+ * @returns filter with default state.
+ */
+export const resetFilter = (): Filters => {
+  return {
+    set: "all",
+    field: AircraftSearchOptions.Model,
+    search: "",
+  };
 };
