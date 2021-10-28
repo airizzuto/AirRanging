76f0d0a496bc4cec21138406ebb87eaaa7604971
airizzuto <agustinrizzuto@hotmail.com>
2021-10-28 16:46:31 -0300

    Added: contact page

---
+++ client/src/components/Pages/Contact/Contact.tsx
@@ -0,0 +1,15 @@
+import InfoPageWrapper from "../../InfoPages/InfoPageWrapper";
+
+const Contact = () => {
+  return (
+    <InfoPageWrapper>
+        <h1>Contact Me</h1>
+        <p>You can contact me at the following pages:</p>
+        <p>Email: <a>airizzuto@gmail.com</a></p>
+        <p>Github: <a href={"https://github.com/airizzuto"}>github.com/airizzuto</a></p>
+        <p>LinkedIn: <a href={"https://linkedin.com/airizzuto"}>linkedin.com/airizzuto</a></p>
+    </InfoPageWrapper>
+  );
+};
+
+export default Contact;
