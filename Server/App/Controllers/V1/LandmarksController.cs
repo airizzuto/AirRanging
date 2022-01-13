27877ddc48680e741750cd18023930a6e5b8d0ec
Agustín I. Rizzuto <60024435+airizzuto@users.noreply.github.com>
2022-01-13 15:23:51 -0300

    Added: controller scaffolded

---
+++ Server/App/Controllers/V1/LandmarksController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers.V1
+{
+
+
+    /// <summary>
+    /// Landmark model controller endpoints:
+    /// <para> GetAllLandmarks           - GET    -  api/landmarks            </para>
+    /// <para> GetAllLandmarksPaginated  - GET    -  api/landmarks/paginated  </para>
+    /// <para> SearchLandmarks           - GET    -  api/landmarks/all        </para>
+    /// <para> GetLandmarkOwnedByUser    - GET    -  api/landmarks/owned      </para>
+    /// <para> GetLandmarkSavedByUser    - GET    -  api/landmarks/saved      </para>
+    /// <para> GetLandmarkId             - GET    -  api/landmarks/5          </para>
+    /// <para> PartialUpdateLandmarkId   - PUT    -  api/landmarks/5          </para>
+    /// <para> CreateLandmark            - POST   -  api/landmarks            </para>
+    /// <para> CloneLandmarkId           - POST   -  api/landmarks/5/clone    </para>
+    /// <para> FullUpdateLandmarkId      - PATCH  -  api/landmarks/5          </para>
+    /// <para> DeleteLandmarkId          - DELETE -  api/landmarks/5          </para>
+    /// </summary>
+
+    [ApiController]
+    [Route("/api/landmarks")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [ApiVersion("1.0")]
+    public class LandmarksController
+    {
+            // TODO: landmark controller
+            // TODO: refactor endpoint to api/{userid}/landmark/[action]?
+    }
+}
