d78cb2b2c7869e8d93beefa5e8d914a944865d41
Agustín I. Rizzuto <60024435+airizzuto@users.noreply.github.com>
2022-01-14 14:53:05 -0300

    Updated: landmark filters

--- Server/Repository/LandmarkRepository.cs
+++ Server/Repository/LandmarkRepository.cs
@@ -4,6 +4,7 @@ using Entities.Models.Landmarks;
 using Entities.Helpers;
 using Entities.Helpers.Landmarks;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Repository
 {
@@ -36,20 +37,20 @@ namespace Repository
 
         // TODO: repo actions
 
-        // public IEnumerable<Landmark> FilterLandmarks(IEnumerable<Landmark> landmarks, LandmarkParameters parameters)
-        // {
-        //     return _filterHelper.ApplyFilter(landmarks, parameters);
-        // }
+        public IEnumerable<Landmark> FilterLandmarks(IEnumerable<Landmark> landmarks, LandmarkParameters parameters)
+        {
+            return _filterHelper.ApplyFilter(landmarks, parameters);
+        }
 
-        // public IEnumerable<Landmark> SortLandmarks(IEnumerable<Landmark> landmarks, LandmarkParameters parameters)
-        // {
-        //     var queryableLandmarks = landmarks.AsQueryable();
-        //     return _sortHelper.ApplySort(queryableLandmarks, parameters.OrderBy);
-        // }
+        public IEnumerable<Landmark> SortLandmarks(IEnumerable<Landmark> landmarks, LandmarkParameters parameters)
+        {
+            var queryableLandmarks = landmarks.AsQueryable();
+            return _sortHelper.ApplySort(queryableLandmarks, parameters.OrderBy);
+        }
 
-        // public IEnumerable<Landmark> PaginatedLandmarks(IEnumerable<Landmark> landmarks, LandmarkParameters parameters)
-        // {
-        //     return _paginationHelper.ApplySort(landmarks, parameters);
-        // }
+        public IEnumerable<Landmark> PaginatedLandmarks(IEnumerable<Landmark> landmarks, LandmarkParameters parameters)
+        {
+            return _paginationHelper.ApplyPagination(landmarks, parameters);
+        }
     }
 }
