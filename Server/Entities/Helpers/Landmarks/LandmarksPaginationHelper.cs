d80a96b6799acc982bd82e7f208d5cfa74237006
Agustín I. Rizzuto <60024435+airizzuto@users.noreply.github.com>
2022-01-14 16:12:49 -0300

    Updated: implemented pagination helper

--- Server/Entities/Helpers/Landmarks/LandmarksPaginationHelper.cs
+++ Server/Entities/Helpers/Landmarks/LandmarksPaginationHelper.cs
@@ -1,7 +1,20 @@
+using System.Collections.Generic;
+using Entities.Models.Landmarks;
+using Entities.Models.Pagination;
+
 namespace Entities.Helpers.Landmarks
 {
-    public class LandmarksPaginationHelper
+    public class LandmarksPaginationHelper : ILandmarksPaginationHelper
     {
-        // TODO:
+        public PagedList<Landmark> ApplyPagination(IEnumerable<Landmark> landmarks, LandmarkParameters parameters)
+        {
+            var pagedLandmarks = PagedList<Landmark>.ToPagedList(
+                landmarks,
+                parameters.PageNumber,
+                parameters.PageSize
+            );
+
+            return pagedLandmarks;
+        }
     }
 }
