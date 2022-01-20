12e810a3b08dc327e684ce4ffd1125a62ac7091f
Agustín I. Rizzuto <60024435+airizzuto@users.noreply.github.com>
2022-01-20 14:58:20 -0300

    Updated: non auth get actions

--- Server/App/Controllers/V1/LandmarksController.cs
+++ Server/App/Controllers/V1/LandmarksController.cs
@@ -1,14 +1,18 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Newtonsoft.Json;
 using AutoMapper;
 using Contracts;
 using Logger;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+using Entities.DTOs.V1.Landmarks;
+using Entities.Models.Landmarks;
 
 namespace App.Controllers.V1
 {
-
-
     /// <summary>
     /// Landmark model controller endpoints:
     /// <para> GetAllLandmarks           - GET    -  api/landmarks            </para>
@@ -28,7 +32,7 @@ namespace App.Controllers.V1
     [Route("/api/landmarks")]
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [ApiVersion("1.0")]
-    public class LandmarksController
+    public class LandmarksController : ControllerBase
     {
         private readonly ILoggerManager _logger;
         private readonly IRepositoryWrapper _repository;
@@ -41,7 +45,94 @@ namespace App.Controllers.V1
       _mapper = mapper;
     }
     
-    // TODO: landmark controller
+    // GET api/landmarks
+        /// <summary>
+        /// Retrieves all landmarks in the database
+        /// </summary>
+        /// <response code="200">Retrieves all landmarks in the database</response>
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<LandmarkReadDTO>>> GetAllLandmarks()
+        {
+            var landmarks = await _repository.Landmark.GetAllLandmarksAsync();
+
+            var landmarksResponse = _mapper.Map<IEnumerable<LandmarkReadDTO>>(landmarks);
+
+            _logger.LogInfo(
+                $"Returning all {landmarksResponse.Count()} landmarks from db."
+            );
+
+            return Ok(landmarksResponse);
+        }
+
+        // GET api/landmarks/paginated
+        /// <summary>
+        /// Retrieves all landmarks in the database
+        /// </summary>
+        /// <response code="200">Retrieves all landmarks in the database</response>
+        [HttpGet("paginated")]
+        [AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<LandmarkReadDTO>>> GetAllLandmarksPaginated([FromQuery] LandmarkParameters parameters)
+        {
+            var landmarks = await _repository.Landmark.GetAllLandmarksAsync();
+
+            var landmarksSorted = _repository.Landmark.SortLandmarks(landmarks, parameters);
+
+            var landmarksPaginated = _repository.Landmark.PaginatedLandmarks(landmarksSorted, parameters);
+
+            var landmarksResponse = _mapper.Map<IEnumerable<LandmarkReadDTO>>(landmarksPaginated);
+
+            _logger.LogInfo(
+                $"Returning all {landmarksResponse.Count()} landmarks from db."
+            );
+
+            return Ok(landmarksResponse);
+        }
+
+        // GET api/landmarks/search
+        /// <summary>
+        /// Retrieves all landmarks in the database
+        /// </summary>
+        /// <response code="200">Retrieves all landmarks in the database</response>
+        [HttpGet("all")]
+        [AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<LandmarkReadDTO>>> SearchLandmarks(
+            [FromQuery] LandmarkParameters parameters)
+        {
+            var landmarks = await _repository.Landmark.GetAllLandmarksAsync();
+
+            var landmarksFiltered = _repository.Landmark.FilterLandmarks(landmarks, parameters);
+
+            var landmarksSorted = _repository.Landmark.SortLandmarks(
+                landmarksFiltered, parameters
+            );
+
+            var landmarksPaginated = _repository.Landmark.PaginatedLandmarks(
+                landmarksSorted, parameters
+            );
+
+            var metadata = new
+            {
+                landmarksPaginated.TotalCount,
+                landmarksPaginated.PageSize,
+                landmarksPaginated.CurrentPage,
+                landmarksPaginated.TotalPages,
+                landmarksPaginated.HasNext,
+                landmarksPaginated.HasPrevious
+            };
+
+            Response.Headers.Add(
+                "X-Pagination", JsonConvert.SerializeObject(metadata)
+            );
+
+            var landmarksResponse = _mapper.Map<IEnumerable<LandmarkReadDTO>>(landmarksPaginated);
+
+            _logger.LogInfo(
+                $" Returning search for all '{landmarksResponse.Count()}' landmarks."
+            );
+
+            return Ok(landmarksResponse);
+        }
     // TODO: refactor endpoint to api/{userid}/landmark/[action]?
   }
 }
